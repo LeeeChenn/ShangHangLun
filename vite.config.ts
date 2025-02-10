3192998d672fce51d1db12caf1237bc8b0eef806
LeeeChenn <lee.chen.art@gmail.com>
2025-02-10 21:20:09 +0800

    根路径

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // 打包后文件的根路径
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
